4f577c22060da90ed20dce0c24e8cd6bc463ec8b
subas-roy <subasroy46@gmail.com>
2025-04-09 18:12:15 +0600

    Check user form token

--- BikeShop-Server/src/module/Products/product.route.ts
+++ BikeShop-Server/src/module/Products/product.route.ts
@@ -1,16 +1,13 @@
 import express from 'express';
 import { ProductController } from './product.controller';
+import auth from '../../app/middlewares/auth';
 
 const router = express.Router();
 
-
-router.post(
-  '/create-product',
-  ProductController.createProduct,
-);
+router.post('/create-product', ProductController.createProduct);
 router.get('/:product', ProductController.getSingleProduct);
 router.delete('/:product', ProductController.deleteProduct);
 router.put('/:product', ProductController.updateProduct);
-router.get('/', ProductController.getProduct);
+router.get('/', auth(), ProductController.getProduct);
 
 export const ProductRouters = router;
