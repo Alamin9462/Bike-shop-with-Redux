68e69c283f9e8c4d419b9710b1381db341b72567
subas-roy <subasroy46@gmail.com>
2025-04-09 10:07:52 +0600

    Implemented business logic validation in auth service

--- BikeShop-Server/src/module/Auth/auth.service.ts
+++ BikeShop-Server/src/module/Auth/auth.service.ts
@@ -1,7 +1,30 @@
+import AppError from '../../app/errors/AppError';
+import UserModel from '../User/user.model';
 import { TLoginUser } from './auth.interface';
+import httpStatus from 'http-status';
 
 const loginUser = async (payload: TLoginUser) => {
   console.log(payload);
+
+  // checking if the user is exist
+  const user = await UserModel.findOne({ email: payload?.email });
+  console.log(user);
+
+  if (!user) {
+    throw new AppError(httpStatus.NOT_FOUND, 'This user is not found!');
+  }
+
+  // checking if the password is correct
+  const isPasswordMatched = await UserModel.findOne({
+    password: payload?.password,
+  });
+
+  if (!isPasswordMatched) {
+    throw new AppError(httpStatus.FORBIDDEN, 'Password does not matched!');
+  }
+
+  // Access Granted: Send AccessToken, RefereshToken
+
   return {};
 };
 
