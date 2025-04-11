cbf43b1a62953c1dae26092300bec3a4a4bad1e8
subas-roy <subasroy46@gmail.com>
2025-04-11 06:58:21 +0600

    Create refresh token when login and sent it to client/cookie

--- BikeShop-Server/src/module/Auth/auth.controller.ts
+++ BikeShop-Server/src/module/Auth/auth.controller.ts
@@ -1,3 +1,4 @@
+import config from '../../app/config';
 import catchAsync from '../../app/utils/catchAsync';
 import sendResponse from '../../app/utils/sendResponse';
 import { AuthServices } from './auth.service';
@@ -17,13 +18,19 @@ const registationUser = catchAsync(async (req, res) => {
 const loginUser = catchAsync(async (req, res) => {
   const payload = req.body;
   const result = await AuthServices.login(payload);
+  const { refreshToken, tokenGenarate } = result;
+
+  res.cookie('refreshToken', refreshToken, {
+    secure: config.NODE_ENV === 'production',
+    httpOnly: true,
+  });
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
     token: result.tokenGenarate,
     message: 'User is logged in successfully',
-    data: result?.user,
+    data: { tokenGenarate },
   });
 });
 
-export const AuthControllers = { loginUser,registationUser };
+export const AuthControllers = { loginUser, registationUser };
--- BikeShop-Server/src/module/Auth/auth.service.ts
+++ BikeShop-Server/src/module/Auth/auth.service.ts
@@ -1,56 +1,73 @@
 import AppError from '../../app/errors/AppError';
 import { IUser } from '../User/user.interface';
 import UserModel from '../User/user.model';
-import bcrypt from "bcrypt";
-import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import httpStatus from 'http-status';
 import config from '../../app/config';
 
 const register = async (payload: IUser) => {
-  const result = await UserModel.create(payload)
+  const result = await UserModel.create(payload);
   return result;
-} 
+};
 
-const login = async (payload : {email:string, password:string}) => {
-  const user = await UserModel.findOne({ email: payload?.email }).select('+password');
-  
+const login = async (payload: { email: string; password: string }) => {
+  const user = await UserModel.findOne({ email: payload?.email }).select(
+    '+password',
+  );
+
   if (!user) {
     throw new AppError(httpStatus.NOT_FOUND, 'This user is not found!');
   }
-  
-  // checking the user active 
+
+  // checking the user active
 
   const userStatus = user?.userStatus;
   if (userStatus === 'inactive') {
-    throw new Error('This user is blocked Account!!')
+    throw new Error('This user is blocked Account!!');
   }
 
-  // password checking 
+  // password checking
 
   const matchedPassword = await bcrypt.compare(
     payload?.password,
-    user?.password
-  )
- 
-  if(!matchedPassword){
-    throw new AppError(httpStatus.FORBIDDEN,'invalid password! Please Enter currect Password')
+    user?.password,
+  );
+
+  if (!matchedPassword) {
+    throw new AppError(
+      httpStatus.FORBIDDEN,
+      'invalid password! Please Enter currect Password',
+    );
   }
 
-  // create jwt token 
+  // create jwt token
 
   const jwtTokenPayload = {
     email: user?.email,
     role: user?.role,
-  }
+  };
 
-  const tokenGenarate = jwt.sign(jwtTokenPayload, config.jwt_access_secret as string, {expiresIn: '5d'});
- 
+  const tokenGenarate = jwt.sign(
+    jwtTokenPayload,
+    config.jwt_access_secret as string,
+    { expiresIn: '5d' },
+  );
+
+  const refreshToken = jwt.sign(
+    jwtTokenPayload,
+    config.jwt_refresh_secret as string,
+    { expiresIn: '365d' },
+  );
+
   return {
-    tokenGenarate, user
-  }
+    tokenGenarate,
+    refreshToken,
+    user,
+  };
 };
 
 export const AuthServices = {
   register,
-  login
+  login,
 };
