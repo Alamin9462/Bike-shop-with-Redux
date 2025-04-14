3fd920ac90ecec5ef6da14c2079d2474df64c96c
subas-roy <subasroy46@gmail.com>
2025-04-14 09:33:37 +0600

    Error handling and resolve type errors

--- BikeShop-Client/src/page/admin/bikes/AddNewBike.tsx
+++ BikeShop-Client/src/page/admin/bikes/AddNewBike.tsx
@@ -6,11 +6,14 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { bikeSchema } from '../../../schemas/bikeSchema';
 import { useAddBikeMutation } from '../../../redux/features/admin/bike.api';
 import { toast } from 'sonner';
+import { TResponse } from '../../../types/global';
 
 const AddNewBike = () => {
   const [addBike] = useAddBikeMutation();
 
   const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const toastId = toast.loading('Creating...');
+
     const bikeData = {
       name: data.name,
       brand: data.brand,
@@ -20,24 +23,28 @@ const AddNewBike = () => {
     };
 
     try {
-      console.log(bikeData);
-      const res = await addBike(bikeData);
-      console.log(res);
+      const res = (await addBike(bikeData)) as TResponse;
+      if (res.error) {
+        toast.error(res.error.data.message, { id: toastId });
+        console.log(res);
+      } else {
+        toast.success('Bike added', { id: toastId });
+      }
     } catch (error) {
-      toast.error('Something went wrong');
+      toast.error('Something went wrong', { id: toastId });
     }
   };
 
   return (
     <Flex justify="center">
       <Col span={6}>
-        <BSFrom onSubmit={onSubmit}>
-          {/*resolver={zodResolver(bikeSchema) */}
+        <BSFrom onSubmit={onSubmit} resolver={zodResolver(bikeSchema)}>
+          {/*resolver={zodResolver(bikeSchema)} */}
           <BSInput type="text" name="name" label="Name"></BSInput>
           <BSInput type="text" name="brand" label="Brand"></BSInput>
-          <BSInput type="text" name="price" label="Price"></BSInput>
+          <BSInput type="number" name="price" label="Price"></BSInput>
           <BSInput type="text" name="model" label="Model"></BSInput>
-          <BSInput type="text" name="stock" label="Stock"></BSInput>
+          <BSInput type="number" name="stock" label="Stock"></BSInput>
           <Button htmlType="submit">Submit</Button>
         </BSFrom>
       </Col>
--- BikeShop-Client/src/schemas/bikeSchema.tsx
+++ BikeShop-Client/src/schemas/bikeSchema.tsx
@@ -3,7 +3,11 @@ import { z } from 'zod';
 export const bikeSchema = z.object({
   name: z.string({ required_error: 'Name field is required' }),
   brand: z.string({ required_error: 'Brand field is required' }),
-  price: z.string({ required_error: 'Price field is required' }),
+  price: z
+    .string({ required_error: 'Price field is required' })
+    .transform((v) => Number(v)),
   model: z.string({ required_error: 'Model field is required' }),
-  stock: z.string({ required_error: 'Stock field is required' }),
+  stock: z
+    .string({ required_error: 'Stock field is required' })
+    .transform((v) => Number(v)),
 });
