389b4eca210d5ced55fd1dc608691eaab9ea8158
Alamin9462 <alaminmd9462@gmail.com>
2025-05-15 13:05:00 +0600

    admin leayout design

--- BikeShop-Client/src/redux/api/baseApi.ts
+++ BikeShop-Client/src/redux/api/baseApi.ts
@@ -13,8 +13,8 @@ import {
   import { toast } from 'sonner';
 
   const baseQuery = fetchBaseQuery({
-    //baseUrl: 'https://bike-shop-server-nine.vercel.app/api/v1',
-    baseUrl: 'http://localhost:5000/api/v1',
+    baseUrl: 'https://bike-shop-server-nine.vercel.app/api/v1',
+    //baseUrl: 'http://localhost:5000/api/v1',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as RootState).auth.token;
@@ -45,8 +45,8 @@ import {
       
       // console.log('Sending refresh token');
   
-     // const res = await fetch('https://bike-shop-server-nine.vercel.app/api/v1/auth/refresh-token', {
-      const res = await fetch('http://localhost:5000/api/v1/auth/refresh-token', {
+      const res = await fetch('https://bike-shop-server-nine.vercel.app/api/v1/auth/refresh-token', {
+      //const res = await fetch('http://localhost:5000/api/v1/auth/refresh-token', {
         method: 'POST',
         credentials: 'include',
       });
